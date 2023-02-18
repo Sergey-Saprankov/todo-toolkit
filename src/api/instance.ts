1ed1816c05be446565f35ba5b62b090a214e5a3e
Sergey-Saprankov <sergey-saprankov@mail.ru>
2023-02-18 11:30:23 +0300

    fix

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
   baseURL: `https://social-network.samuraijs.com/api/1.1`,
   withCredentials: true,
   headers: {
-    "API-KEY": "26610f0b-dfa3-4af0-8c0f-815bd7226b14",
+    "API-KEY": "c53710fa-3381-4f82-a43d-14117775329a",
   },
 });
